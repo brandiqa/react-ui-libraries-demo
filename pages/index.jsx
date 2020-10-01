0e67047f514802b1ce3c067415d829909a08437f
Michael Wanyoike <micwanyoike@gmail.com>
2020-10-01 16:19:45 +0300

    - Removed existing styles - Implemented Prettier , .editorconfig - Lint/Formatted files - Renamed .js components to .jsx - Implemented users api (list only)

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Bulma CSS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Bulma CSS Demo</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
